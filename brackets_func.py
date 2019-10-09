96319614da4358e00393c1bcfe03e39fcbbfda8f
Kinosa777 <56258724+Kinosa777@users.noreply.github.com>
2019-10-09 17:28:42 +0300

    Update brackets_func.py

--- brackets_func.py
+++ brackets_func.py
@@ -1,6 +1,12 @@
 text = 'ff()(e)(d()(sd()323))'
 
 def balance_brckts(brack_txt):
+'''
+    Checks if the balance and the order of brackets ()
+    in a string is correct
+    
+    Returns: "Error" or "OK"
+'''
     bal_br_res = "OK"
     n = bal = 0
     while bal >= 0 and n < len(brack_txt):
