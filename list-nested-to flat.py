8e59342c36e6dd553685a597f4e82f04a1646c95
Kinosa777 <56258724+Kinosa777@users.noreply.github.com>
2019-10-09 16:06:52 +0300

    Update list-nested-to flat.py

--- list-nested-to flat.py
+++ list-nested-to flat.py
@@ -1,6 +1,15 @@
-l1 = [[[1, 2.9], 8], "r", [3, 5], 4]
-t1 = str(l1)
-t1 = t1.replace("[", "")
-t1 = t1.replace("]", "")
-l2 = list(t1.split(", "))
-#всі елементи списку тепер на одному рівні, але текстового типу
+lst = [[[1, 2.9], 8], 'r', [3, 5], 4]
+
+
+def flatten(arr):
+    for elem in arr:
+        if isinstance(elem, list):
+            flatten(elem)
+        else:
+            flat.append(elem)
+    return (flat)
+
+
+flat = []
+flatten(lst)
+print(flat)
