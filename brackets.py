1d0ad8ff03858e568baa99d3f8ac8698f395bd02
Kinosa777 <56258724+Kinosa777@users.noreply.github.com>
2019-10-09 16:40:09 +0300

    Update brackets.py

--- brackets.py
+++ brackets.py
@@ -1,17 +1,18 @@
-from colorama import init
-init()
-from colorama import Back
-text = 'ff())(e))(d()(sd()3))23)'
-n = bal = 0
-while bal >= 0 and n < len(text):
-    if text[n] == '(':
-        bal += 1
-    elif text[n] == ')':
-        bal -= 1
-    n += 1
-if bal == 0:
-    print('OK')
-elif bal < 0:
-    print('Error: ' + text[0:n - 1] + Back.RED + text[n - 1] + Back.RESET + text[n:len(text)])
-else:
-    print('Error: Missing closing bracket')
+text = 'ff()(e)(d()(sd()323))'
+
+def balance_brckts(brack_txt):
+    bal_br_res = "OK"
+    n = bal = 0
+    while bal >= 0 and n < len(brack_txt):
+        if brack_txt[n] == '(':
+            bal += 1
+        elif brack_txt[n] == ')':
+            bal -= 1
+        n += 1
+    if bal:
+        bal_br_res = "Error"
+    print(bal)
+    return(bal_br_res)
+
+result = balance_brckts(text)
+print(result)
