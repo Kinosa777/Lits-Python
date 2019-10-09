e142bb21a40411c5a58c0ef95ad24713faa8917a
Kinosa777 <56258724+Kinosa777@users.noreply.github.com>
2019-10-09 17:30:45 +0300

    Update list-nested-to flat.py

--- list-nested-to flat.py
+++ list-nested-to flat.py
@@ -1,6 +1,11 @@
 lst = [[[1, 2.9], 8], 'r', [3, 5], 4]
 
 def flatten(arr):
+'''
+    Transforms a nested list into a flat one
+    
+    Returns: flat
+'''
     for elem in arr:
         if isinstance(elem, list):
             flatten(elem)
