d71e54bc156012ff2600dad6abeef296260cfb3d
Kinosa777 <56258724+Kinosa777@users.noreply.github.com>
2019-10-20 18:56:16 +0300

    Update most_frequent.py

--- most_frequent.py
+++ most_frequent.py
@@ -1,5 +1,7 @@
 def find_most_frequent(text):
-    """Returns the most frequently used words used in txt"""
+    """Returns the most frequently used words used in txt.
+    All words are in lower case.
+    If more than one - sorts the list in alphabetical order."""
     text = text.lower()
     for punct in ',.:;!?-':
         text = text.replace(punct, ' ')
@@ -10,13 +12,11 @@ def find_most_frequent(text):
             uses[wrd] = 1
         else:
             uses[wrd] += 1
-    max_value = 0
+    max_value = max(uses.values())
+    frequent = []
     for wrd in uses:
         if uses.get(wrd) == max_value:
             frequent.append(wrd)
-        elif uses.get(wrd) > max_value:
-            frequent = [wrd,]
-            max_value = uses.get(wrd)
     frequent.sort()
     return frequent
 
@@ -25,7 +25,3 @@ print(find_most_frequent('Hello,Hello, my dear!'))
 print(find_most_frequent('to understand recursion you need first to understand recursion...'))
 print(find_most_frequent('Mom! Mom! Are you sleeping?!!!'))
 print(find_most_frequent("Hello,Hello, my dear!"))
-
-
-
-
